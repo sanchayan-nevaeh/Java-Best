f2268b827d041c5847b159b5a9e402c52d652727
Mehir-Nevaeh <121491311+Mehir-Nevaeh@users.noreply.github.com>
2022-12-28 11:27:16 +0530

    Update test.java

--- test.java
+++ test.java
@@ -4,5 +4,6 @@ class test{
     public static void main(String args[]){
         LocalDate d1 = LocalDate.now();
         System.out.println("Good Morning, Current Date is "+d1);
+        System.out.println("Have a Nice Day");
     }
 }
